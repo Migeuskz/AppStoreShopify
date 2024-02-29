fae4fa33451b8232f2f9c14c4bdeea147a87660b
Miguel Angel Sanchez Papalotzi <msanchezpapalot@gmail.com>
2024-02-28 23:31:29 -0600

    agregando nuevas fuentes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 
-import { Inter } from "next/font/google";
+import {Lexend_Deca} from 'next/font/google'
 
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
 import 'app/sass/globals.sass'
-const inter = Inter({ subsets: ["latin"] });
 
 
+const lexend_deca = Lexend_Deca({ 
+  weight: ["100", "300", "500", "700"],
+  subsets: ["latin"]
+ });
 
 export default function RootLayout({
   children,
@@ -15,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={lexend_deca.className} >
         <Header />
 
         {children}
