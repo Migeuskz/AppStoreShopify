c2eba519cb7bf67533178bd17c0bfe6f52468c74
Miguel Angel Sanchez Papalotzi <msanchezpapalot@gmail.com>
2024-02-28 22:49:25 -0600

    agregando estilos y optimizacion de imagenes

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,19 +1,26 @@
 import styles from './Description.module.sass'
+import Image from 'next/image';
 
-export const Description = () =>{
-    return(
+export const Description = () => {
+    return (
         <section className={styles.Description}>
-            <img src="/images/description.jpg" alt="products marketplace" />
+            <Image 
+                src="/images/description.jpg" 
+                alt="products marketplace" 
+                width={800} 
+                height={550}
+                priority = {false}
+                quality={100}
+            />
+            <div>
+                <h2>Descripcion</h2>
 
-            <h2>Descripcion</h2>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore adipisci enim quibusdam quae accusamus fugiat error quos neque libero ea sed accusantium nostrum esse, quis odit beatae voluptatum quam magni.
+                    Veniam architecto voluptatem
+                </p>
+            </div>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque corrupti facilis explicabo accusantium, saepe voluptatibus eum ullam ut, suscipit cupiditate nemo culpa alias temporibus nam, corporis deleniti iure. Saepe, atque.
-            Magnam minus, tempora quod porro non ullam nam quis distinctio voluptates odio ex alias est voluptatibus vero ducimus repellendus quas ipsam eius minima eum similique modi consectetur dolore. Delectus, vitae.
-            Impedit, obcaecati totam animi veniam quae quam veritatis voluptate alias. Voluptate, velit nisi delectus asperiores, molestiae illo illum voluptatem, id perferendis sint dolor. Aliquam sed voluptatem aspernatur exercitationem dolor. Repellendus.
-            Animi quia sed illum itaque harum. Perspiciatis, vitae consectetur non iste doloribus saepe! Possimus alias, pariatur illo nulla exercitationem tempore nam aut eos consectetur at sint eum autem, mollitia commodi?
-            Quibusdam quidem porro animi, non perspiciatis repellat maiores harum saepe nihil illum a dolore et alias quaerat laborum libero vel culpa. Itaque porro doloribus non debitis. Alias voluptates ad suscipit?
-            Dignissimos maxime neque a voluptates quod cupiditate quos atque accusamus ea hic eum debitis est, molestias, inventore quisquam saepe rerum quia numquam exercitationem sit, explicabo impedit vel consectetur delectus? Veritatis!
-            Qui ipsa dolor, tempora explicabo aspernatur quos fuga quia totam nihil eius saepe odio deleniti neque. Sed atque eaque necessitatibus alias unde delectus assumenda consequatur perspiciatis amet ea! Quo, consectetur?</p>
         </section>
     );
 }
