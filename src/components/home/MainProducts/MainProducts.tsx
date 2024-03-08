17486700b5711feff371e7d6eb82594ddef0d392
Miguel Angel Sanchez Papalotzi <msanchezpapalot@gmail.com>
2024-03-08 14:02:45 -0600

    obteniendo datos de la api

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,5 +1,18 @@
-export const MainProducts = () =>{
-    console.log(process.env.SHOPIFY_HOSTNAME)
+
+const getProducts = async () =>{
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-01/products.json`, {
+        headers: new Headers({
+            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+        })
+    })
+    const  data = await response.json()
+    return data
+}
+
+export const MainProducts = async () =>{
+    // console.log(process.env.SHOPIFY_HOSTNAME)
+    const products = await getProducts()
+    console.log(products)
     return(
         <section>
             <h1>Main products</h1>
