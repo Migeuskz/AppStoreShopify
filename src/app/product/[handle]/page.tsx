ed912a6659715de7402b5eccdfe672d13819c985
Miguel Angel Sanchez Papalotzi <msanchezpapalot@gmail.com>
2024-03-29 15:53:07 -0600

    pagina de productos y arreglos de sitio

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,17 +1,20 @@
-"use client"
-import { useParams, useSearchParams } from "next/navigation"
 
+import { ProductView } from "app/components/product/ProductView";
+import { getProducts } from "app/services/shopify/products";
 
+interface ProductPageProps{
+    searchParams: {
+        id: string
+    }
+}
 
-export default function ProductPage() {
-    const  params = useParams();
-    const searchParams = useSearchParams();
-    const id = searchParams.get("id") || params.id;
-    // console.log(params)
+export default async function ProductPage({searchParams}: ProductPageProps) {
+    const id = searchParams.id
+    const products = await getProducts(id)
+    const product = products[0]
+    console.log('product', products)
     console.log('searchParams', id)
     return(
-        <div>
-            <h1>Producto</h1>
-        </div>
+        <ProductView product={product}/>
     )
 }
--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,16 @@
-interface ErrorPageProps{
+interface ErrorPageProps {
     error: Error;
     reset: () => void;
 }
+
+type ProductType = {
+    id: string;
+    title: string;
+    description: string;
+    price: number;
+    image: string;
+    quantity: number;
+    handle: string;
+    tags: string;
+}
+
