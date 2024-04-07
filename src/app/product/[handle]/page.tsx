72f0cd364b43584cd13e4706a2530ad6210d3d05
Miguel Angel Sanchez Papalotzi <msanchezpapalot@gmail.com>
2024-04-07 16:15:17 -0600

    aplicando redirects

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,6 +1,8 @@
 
 import { ProductView } from "app/components/product/ProductView";
 import { getProducts } from "app/services/shopify/products";
+import { redirect } from "next/navigation";
+
 
 interface ProductPageProps{
     searchParams: {
@@ -11,9 +13,13 @@ interface ProductPageProps{
 export default async function ProductPage({searchParams}: ProductPageProps) {
     const id = searchParams.id
     const products = await getProducts(id)
-    const product = products[0]
-    console.log('product', products)
-    console.log('searchParams', id)
+    const product = products[0] 
+
+    // if(!id){
+    //     redirect('/store')
+    // }
+    // console.log('product', products)
+    // console.log('searchParams', id)
     return(
         <ProductView product={product}/>
     )
